84da7d44c9a5b19f5bcbd4ff1bfceb05e0e3ef4f
김소진 <thwls9930@gmail.com>
2023-01-30 14:49:26 +0900

    구조 설계

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <title>포트폴리오</title>
-</body>
-</html>
+  <?php include "header.php" ?>
+  <main class="main">
+    <?php include 'home.php' ?>
+    <?php include 'about.php' ?>
+    <?php include 'work1.php' ?>
+    <?php include 'work2.php' ?>
+    <?php include 'work3.php' ?>
+    <?php include 'work4.php' ?>
+    <?php include 'work5.php' ?>
+    <?php include 'work6.php' ?>
+    <?php include 'contact.php' ?>
+  </main><!-- main -->
+  <?php include 'footer.php' ?>
