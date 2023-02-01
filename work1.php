8fc72e4f51a7a22c352277ee10adbb261d73a700
DESKTOP-PH7JJ39\tj-bu-04 <pass2149@gmail.com>
2023-02-01 12:08:47 +0900

    work 수정 학원

--- work1.php
+++ work1.php
@@ -3,9 +3,40 @@
     <div class="work-info">
       <div class="mockup">
 
+
       </div><!-- mockup -->
       <div class="mockup-desc">
-
+        <div class="title">
+          <h1>제목</h1>
+          <p>부제목</p>
+        </div><!-- title -->
+        <ul>
+          <li>
+            <span>TYPE</span>
+            <span>내용</span>
+          </li>
+          <li>
+            <span>PAGE</span>
+            <span>내용</span>
+          </li>
+          <li>
+            <span>TOOL</span>
+            <span>내용</span>
+          </li>
+          <li>
+            <span>PROCESS</span>
+            <span>내용</span>
+          </li>
+          <li>
+            <span>참여도</span>
+            <span>내용</span>
+          </li>
+        </ul>
+        <div class="link">
+          <a href="#">Styleguide</a>
+          <a href="#">Prototype</a>
+          <a href="#">Prototype</a>
+        </div>
       </div><!-- mockup-desc -->
     </div><!-- work-info -->
   </div><!-- work1 -->
