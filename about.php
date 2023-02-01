e9449214af589ef4bdacde85e8569ec41ffce0cf
DESKTOP-2FA7701\82106 <thwls9930@gmail.com>
2023-02-01 21:16:43 +0900

    about 구조 수정

--- about.php
+++ about.php
@@ -8,52 +8,66 @@
 
       <ul class="profile-info">
         <li>
-          <p>생일</p>
-          <p>1998.02.23</p>
+          <p>
+            <i class="fa-solid fa-calendar-days"></i>
+            1998.02.23
+          </p>
         </li>
         <li>
-          <p>핸드폰번호</p>
-          <p>010-6313-9930</p>
+          <p>
+            <i class="fa-solid fa-mobile-screen-button"></i>
+            010-6313-9930
+          </p>
         </li>
         <li>
-          <p>이메일</p>
-          <p>thwis9930@gmail.com</p>
+          <p>
+            <i class="fa-regular fa-envelope"></i>
+            thwis9930@gmail.com
+          </p>
         </li>
       </ul><!-- info-all -->
 
       <ul class="edu">
         <li>
-          <p>2022-10-17 - 202301-19</p>
-          <p>프로젝트기반 웹퍼블리셔(웹디자인과정)</p>
+          <em>2022-10-17 - 2023-01-19</em>
+          <p>프로젝트기반 웹퍼블리셔(웹디자인)과정</p>
+        </li>
+        <li>
+          <em>2021-12-08 - 2022-01-12</em>
+          <p>프로그래밍 첫걸음 시작하기[초급]</p>
+        </li>
+        <li>
+          <em>2020-07-20 ~ 2020-09-22</em>
+          <p>디자인툴로 상세페이지 및 쇼핑몰제작</p>
         </li>
       </ul><!-- edu -->
 
     </div><!-- contents-left-wrap 왼쪽-->
 
     <div class="contents-right-wrap">
-      <ul class="name">
-        <li>
-          <p>kim so Jin</p>
-          <p>WEB PUBLISHER</p>
-        </li>
-      </ul><!-- name -->
-      <ul class="profile-info">
-        <li>
-          <p>"다채로운 퍼블리셔 김소진입니다."</p>
-        </li>
-
-        <li>
-          <p>내용입력</p>
-        </li>
-
-        <li>
-          <p>내용입력</p>
-        </li>
-
-        <li>
-          <p>내용입력</p>
-        </li>
-      </ul><!-- profile-info -->
+      <h2 class="name">
+        <p>kim so Jin</p>
+        <small>WEB PUBLISHER</small>
+      </h2><!-- name -->
+      <div class="introduce">
+        <p>"다채로운 퍼블리셔 김소진입니다."</p>
+        <p>
+          CSS와 Javascript를 이용해 모션을 주고
+          동적인 화면을 구사하는 것에 관심이 많으며,
+          사용자에게 다채로운 ui/ux를 제공하고,
+          퍼블리싱을 통해 사용자의 시각이 즐거워지고
+          흥미로운 경험을 제공하고 싶습니다.
+        </p>
+        <p>
+          개발은 혼자 하는 일이 아닌 만큼 사람들 간의 약속, 그리고 소통이 중요하다고 생각합니다
+          항상 협업과 유지보수에 좋은 코드를 짜기 위해 꾸준히 공부하고 있습니다.
+          유연한 소통을 할 수 있도록, 항상 경청하고 열린 자세로 임하며 성장해 나가겠습니다.</p>
+        <p>
+          경험을 토대로 더 많은 것을 배우고,
+          배운것을 토대로 어떤 변화에도 적극적으로 임하고,
+          웹을 트렌디하게 표현해내는 퍼블리셔가 되겠습니다.
+        </p>
+      </div><!-- profile-info -->
     </div><!-- contents-right-wrap  오른쪽-->
 
   </div><!-- about-profile 전체-->
