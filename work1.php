f4946d93c4a34224f191e3374d06b38c551ca925
DESKTOP-KHSDQ4J\최준혁 <pass2149@gmail.com>
2023-02-05 22:48:14 +0900

    집 work1 수정

--- work1.php
+++ work1.php
@@ -1,10 +1,25 @@
 <section class="work1-section">
   <div class="work1">
-    <div class="work-info">
-      <div class="mockup">
-      
+    <div class="work-contents">
+      <div class="mockup-all">
 
-      </div><!-- mockup -->
+        <div class="mockup-c">
+          <div class="screenshot-c">
+            <img src="" alt=""><!-- 컴퓨터안 이미지 -->
+            <p class="frame-c"></p>
+          </div><!-- screenshot -->
+        </div><!-- mockup-c -->
+
+        <div class="mockup-m">
+          <div class="screenshot-m">
+            <img src="" alt=""><!-- 컴퓨터안 이미지 -->
+            <p class="frame-m"></p>
+          </div><!-- screenshot -->
+        </div><!-- mockup-m -->
+
+      </div><!-- mockup-all -->
+
+
       <div class="mockup-desc">
         <div class="title">
           <h1>제목</h1>
@@ -38,6 +53,6 @@
           <a href="#">Prototype</a>
         </div>
       </div><!-- mockup-desc -->
-    </div><!-- work-info -->
+    </div><!-- work-contents -->
   </div><!-- work1 -->
 </section><!-- section-work1 -->
