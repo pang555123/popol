38cf91f1e64e88345f5135e00fd221a76dd0fff3
sowon <thwls9930@gmail.com>
2023-02-09 00:51:00 +0900

    푸터 작업

--- footer.php
+++ footer.php
@@ -1,7 +1,15 @@
 <link rel="stylesheet" href="./css/footer.css">
 <footer>
-  <div class="footer-section scr-section scr-section10 scr-section10-scr " data="10"></div>
-
+  <div class="footer-section scr-section scr-section10 scr-section10-scr " data="10">
+    <div class="flow-container">
+      <div class="flow-text">
+        <div class="flow-wrap">Thank You For Watching My Portfolio </div>
+        <div class="flow-wrap">Thank You For Watching My Portfolio </div>
+        <div class="flow-wrap">Thank You For Watching My Portfolio </div>
+        <div class="flow-wrap">Thank You For Watching My Portfolio </div>
+      </div>
+    </div>
+  </div>
 </footer>
 </body>
 
