c5d71b15a20eb00aa468edf3f7c92049bd4cc883
sowon <thwls9930@gmail.com>
2023-02-20 15:16:18 +0900

    active 범위 수정

--- js/active.js
+++ js/active.js
@@ -1,8 +1,12 @@
 $(function(){
   function fnactive() {
     $(`.contact-section .contact-container ,.about-section .about-profile`).each(function () {
-      t = $(this).offset().top// 객체와 윈도우탑의 거리
-      if (scrY >= t - winH * 0.9 && scrY <= t - winH * 0.05) {
+      var offset = $(this).offset().top// 객체와 윈도우탑의 거리
+      var innerH = $(this).innerHeight()
+      if (
+        (scrY >= offset - winH * 0.1 + innerH) -
+        (scrY >= offset - winH * 0.9)
+        ) {
         $(this).addClass('active')
       } else {
         $(this).removeClass('active')
